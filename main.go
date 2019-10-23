6db462e9d2d05885888b6bd1768e771031e1810b
Aren R Windham <windham.aren@protonmail.com>
2019-10-23 14:28:12 -0700

    uses env variables now

--- main.go
+++ main.go
@@ -21,15 +21,16 @@ func makeProjectCounter() func() (int, string) {
 
 func main() {
 	if len(os.Args) < 4 {
-		fmt.Println("usage: ./automatic-octo-computing-machine api-uid api-secret user")
+		fmt.Println("usage: ./automatic-octo-computing-machine user")
+		fmt.Println("environment variables: user_id, user_secret")
 		os.Exit(1)
 	}
 
-	uid := os.Args[1]
-	secret := os.Args[2]
+	uid := os.Getenv("user_id")
+	secret := os.Getenv("user_secret")
 	username := os.Args[3]
 	client := apiwrap.NewClient(uid, secret)
-	fmtstr := "/v2/users/%d/projects_users?filter[project_id]=%d"
+	fmtstr := "/v2/users/%d/projects_users?filter[project_id]=%d&page[size]=100"
 
 	userID, err := client.GetUserID(username)
 	if err != nil {
@@ -53,7 +54,7 @@ func main() {
 
 			fmt.Println(fmt.Sprintf("%s: %f", projectName, dat[0]["final_mark"]))
 		}
-		if projectID == 5 {
+		if projectID == 5 { // id of ft_printf
 			os.Exit(0)
 		}
 		time.Sleep(time.Second) // sleep for api rate limit
